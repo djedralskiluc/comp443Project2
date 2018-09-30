f294c695e97d9988f7a650693efd5e23fb9a49e4
Dariusz <djedralski@ctlglobalsolutions.com>
2018-09-30 16:06:58 -0500

    updated Address

--- ecomerce/src/com/company/ecomerce/customer/Address.java
+++ ecomerce/src/com/company/ecomerce/customer/Address.java
@@ -1,40 +1,48 @@
 package com.company.ecomerce.customer;
 
 public class Address {
-	private String line1;
-	private String line2;
-	private String line3;
-	
-	public Address(String line1,String line2, String line3) {
-		this.setLine1(line1);
-		this.setLine2(line2);
-		this.setLine3(line3);
+	private String Attn;
+	private String Addr1;
+	private String Addr2;
+	private String City;
+	private String State;
+	private String Zip;
+	public String getAttn() {
+		return Attn;
+	}
+	public void setAttn(String attn) {
+		Attn = attn;
+	}
+	public String getAddr1() {
+		return Addr1;
+	}
+	public void setAddr1(String addr1) {
+		Addr1 = addr1;
+	}
+	public String getAddr2() {
+		return Addr2;
+	}
+	public void setAddr2(String addr2) {
+		Addr2 = addr2;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getZip() {
+		return Zip;
+	}
+	public void setZip(String zip) {
+		Zip = zip;
 	}
 	
-
-
-	public String getLine1() {
-		return line1;
-	}
-
-	public void setLine1(String line1) {
-		this.line1 = line1;
-	}
-
-	public String getLine3() {
-		return line3;
-	}
-
-	public void setLine3(String line3) {
-		this.line3 = line3;
-	}
-
-	public String getLine2() {
-		return line2;
-	}
-
-	public void setLine2(String line2) {
-		this.line2 = line2;
-	}
 }
 
